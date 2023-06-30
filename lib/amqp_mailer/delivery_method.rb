f5c8a7b966bc576fd77ab88b0a0eb54ef4420be4
ashutosh <ashutosh.kaul@getsimpl.com>
2023-06-30 15:52:47 +0530

    fix

--- lib/amqp_mailer/delivery_method.rb
+++ lib/amqp_mailer/delivery_method.rb
@@ -43,7 +43,6 @@ module AmqpMailer
           notification_type: 'email',
           notification_id: SecureRandom.uuid
       }
-
       payload.merge!(category: mail['category'].value) if mail['category']
       payload.merge!(critical: mail['critical'].value) if mail['critical']
       payload.merge!(reply_to: mail['reply_to'].value) if mail['reply_to']
